e31cbb55dc84927573efa72de253fe725469d38a
Karsten Westra <karsten@moyosa.nl>
2019-01-09 12:38:00 +0100

    Added rocket with keyboard control for movement and stub environment.

---
+++ Assets/Character/Spaceship/Scripts/KeyboardController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+// ReSharper disable once CheckNamespace
+namespace Character.Spaceship
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class KeyboardController : MonoBehaviour
+    {
+        [SerializeField]
+        private float rcsRotationForce = 32f;
+        
+        [SerializeField]
+        private float rcsThrustForce = 16f;
+     
+        private Rigidbody _rigidBody;
+    
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _rigidBody = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            ProcessInput();
+        }
+
+        private void ProcessInput()
+        {
+            _rigidBody.freezeRotation = true;
+            ProcessThrustInput();
+            ProcessRotationInput();
+            _rigidBody.freezeRotation = false;
+        }
+
+        private void ProcessThrustInput()
+        {
+            if (!Input.GetKey(KeyCode.Space))
+            {
+                return;
+            }
+            _rigidBody.AddRelativeForce(Vector3.up * rcsThrustForce);
+        }
+
+        private void ProcessRotationInput()
+        {
+            var frameRotation = rcsRotationForce * Time.deltaTime;
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Rotate(Vector3.forward * frameRotation);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Rotate(-Vector3.forward * frameRotation);
+            }
+        }
+    }
+}
---
+++ Assets/Character/Spaceship/Scripts/ThrustSound.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+// ReSharper disable once CheckNamespace
+namespace Character.Spaceship
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class ThrustSound : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+    
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            ProcessInput();
+        }
+
+        private void ProcessInput()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (!_audioSource.isPlaying)
+                {
+                    _audioSource.Play();
+                }
+            }
+            else
+            {
+                _audioSource.Stop();
+            }
+        }
+    }
+}
